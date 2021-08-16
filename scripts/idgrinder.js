ba1d7a050935509741f4cb853e4352a43fa0f604
tlras <14915607+tlras@users.noreply.github.com>
2021-08-15 23:16:46 -0500

    Upgrade to version 4.
    version 4 changes:
      * Added a mechanism to prevent two instances from running at the same time.
      * Put the ID grinding code into its own function so it can be called again later instead of rerunning the whole script.

--- scripts/idgrinder.js
+++ scripts/idgrinder.js
@@ -1,4 +1,4 @@
-/* yagton's "ID Grinder" script (version 3.2)
+/* yagton's "ID Grinder" script (version 4)
  * NOTICE: While running this script, your OWOT tab will be effectively frozen
  *         until it's finished, and any chat messages/edits made during this
  *         time may not be received/sent by your client.
@@ -6,37 +6,51 @@
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to <http://unlicense.org/> */
 
-// Set this variable to true to stop the script prematurely.
-var IDG_KILL = false;
-
-// Used by the isGood() function; more info there.
-var COOL_IDS = [420, 6969, 666, 1337, 2021];
+// Keeps track of program state to prevent two copies from running at the
+// same time. Set idg_state to -1 to stop the script prematurely.
+var idg_state = idg_state || 0;
 
 // This function is used to determine if an ID is to be kept. For most people,
 // it's the only part of the script you should need to modify. By default, it
 // returns true if the ID is in the COOL_IDS array or is less than 100.
+var COOL_IDS = [420, 6969, 666, 1337, 2021];
 function isGood(a) {
     return COOL_IDS.includes(a) || a < 100;
 }
 
-// Proxies ws_functions.channel() to intercept when a new ID is received from
-// the server. If the ID is approved by isGood(), the proxy is removed and
-// the original function then runs normally; but if it's not approved, it will
-// run socket.refresh() again and again until it eventually is.
-var old_function = ws_functions.channel;
-
-ws_functions.channel = new Proxy(ws_functions.channel, {
-    apply: function(target, _, args) {
-        if (isGood(args[0].id) || IDG_KILL) {
-            console.log(`Stopping! Your new ID is: ${args[0].id}`);
-            ws_functions.channel = old_function;
-            return target(...args);
-        } else {
-            socket.refresh();
-        }
+// Tip: If you want to start the script again after you've loaded it once,
+// you can just rerun this function; no need to load the whole script again.
+function grindID() {
+    // Make sure there aren't any other instances.
+    if (idg_state != 0) {
+        console.log("An instance of this script is already running! Refusing to start.");
+        return;
     }
-});
+    idg_state = 1;
 
-// Generates a new ID, and due to the code above, it will keep rerunning until
-// you get your desired ID.
-socket.refresh();
+    // Proxies ws_functions.channel() to intercept when a new ID is received from
+    // the server. If the ID is approved by isGood(), the proxy is removed and
+    // the original function then runs normally; but if it's not approved, it will
+    // run newID() again and again until it eventually is.
+    var old_function = ws_functions.channel;
+
+    ws_functions.channel = new Proxy(ws_functions.channel, {
+        apply: function(target, _, args) {
+            if (isGood(args[0].id) || idg_state == -1) {
+                console.log(`Stopping! Your new ID is: ${args[0].id}`);
+                ws_functions.channel = old_function;
+                idg_state = 0;
+                return target(...args);
+            } else {
+                socket.refresh();
+            }
+        }
+    });
+
+    // Generates a new ID, and due to the code above, it will keep rerunning until
+    // you get your desired ID.
+    socket.refresh();
+}
+
+// Begin grinding a new ID.
+grindID();
