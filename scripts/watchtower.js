4bbf88af6531edd70542ae20ce2136b53e434e1e
tlras <14915607+tlras@users.noreply.github.com>
2021-09-27 04:52:37 +0000

    Update watchtower.js
    This version uses a Proxy object in order to reuse this existing WebSocket instead of creating a new one; this should be slightly more efficient than the previous version.

--- scripts/watchtower.js
+++ scripts/watchtower.js
@@ -1,4 +1,4 @@
-/* yagton's "Watchtower" script (version 1.1)
+/* yagton's "Watchtower" script (version 2)
  * This is a simple script that opens a WebSocket and listens for tileUpdate messages.
  * After it recieves such a message, it will output the location of the update to the console.
  * The purpose of this script is so you can see where edits are happening on an OWOT world.
@@ -9,16 +9,19 @@
  * 
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to <http://unlicense.org/> */
-var socket = new WebSocket(ws_path);
 
-socket.onmessage = function (a) {
-  var data = JSON.parse(a.data);
-  if (data.kind == "tileUpdate") {
-    for (const i in data.tiles) {
-      var nums = i.split(',').map(x => parseInt(x));
-      var positionX = Math.floor(nums[1] / coordSizeX);
-      var positionY = Math.floor(-nums[0] / coordSizeY);
-      console.log(`tileUpdate at ${positionX}, ${positionY}.`);
+w.socket.onmessage = new Proxy(w.socket.onmessage, {
+  apply: function(func, _, args) {
+    var data = JSON.parse(args[0].data);
+    if (data.kind == "tileUpdate") {
+      for (const i in data.tiles) {
+        var nums = i.split(',').map(x => parseInt(x));
+        var positionX = Math.floor(nums[1] / coordSizeX);
+        var positionY = Math.floor(-nums[0] / coordSizeY);
+        console.log(`tileUpdate at ${positionX}, ${positionY}.`);
+      }
     }
+
+    func(...args);
   }
-}
+});
