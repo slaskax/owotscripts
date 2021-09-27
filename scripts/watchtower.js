583531e9e2a0bef03b9c242d710d7d5eada9ed9d
tlras <14915607+tlras@users.noreply.github.com>
2021-09-27 02:59:58 +0000

    Change wording of a comment.
    NOTE: Since no code was changed, the version number was intentionally not incremented.

--- scripts/watchtower.js
+++ scripts/watchtower.js
@@ -1,7 +1,7 @@
 /* yagton's "Watchtower" script (version 1.1)
  * This is a simple script that opens a WebSocket and listens for tileUpdate messages.
  * After it recieves such a message, it will output the location of the update to the console.
- * The purpose of this script is so you can effectivly see where any edit is happening on an OWOT page.
+ * The purpose of this script is so you can see where edits are happening on an OWOT world.
  * Please do not use this for malicious purposes.
  * 
  * WARNING: This script has a known "off-by-one" bug. I have tried several times to fix it,
