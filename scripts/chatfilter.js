df2de37f1f6dc89abd85c00a73eda6863a0adff7
tlras <14915607+tlras@users.noreply.github.com>
2021-11-17 01:06:49 +0000

    Update to v3.
    It took awhile to release, but now it's here!
    v3 adds these new features:
        * Dynamic configuration via client-side chat commands.
        * Persistent settings between sessions.
    
    In v4 (which should be done in less than a week), I plan on adding synchronization between multiple instances, and built-in documentation via /block help, but I wanted to quickly release this version for people to play around with and bug test.

--- scripts/chatfilter.js
+++ scripts/chatfilter.js
@@ -1,53 +1,261 @@
-/* yagton's "Chat Filter" script (version 2)
- * 
+/* yagton's "Chat Filter" script (version 3)
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to <http://unlicense.org/> */
- 
-// Chat will be filtered based on these rules:
-var filter_rules = {
-    // Any message containing strings in this list will be hidden from chat.
-    string: ["uwu", "owo", "yiff", "hacked", "hehe"],
 
-    // Anybody with a nickname in this list will be hidden from chat.
-    nick: ["hidemymessages"],
+// If this flag is set to true, your block list will persist between runs.
+const CF_PERSIST = true;
 
-    // Any user in this list will be hidden from chat.
-    user: [],
+/* Internally used variable for keeping track of filter rules.
+ * These are the default values for it. They can (and should) be changed
+ * using client commands, not directly. */
+const filter_rules_defaults = "{\"string\":[],\"nick\":[],\"user\":[],\"id\":[]}";
+var filter_rules = JSON.parse(filter_rules_defaults);
 
-    /* Anybody with an ID in this list will be hidden from chat.
-     * NOTE: This must be an integer; if you put a string, it won't work. */
-    id: []
+/**********************************************************
+ * PERSISTENCE AND TAB SYNCHRONIZATION
+ *********************************************************/
+
+if (CF_PERSIST) {
+    // Load existing settings from localStorage, if they exist.
+    filter_rules = Object.assign(
+        filter_rules,
+        JSON.parse(localStorage.getItem("cf_rules"))
+    );
 }
+
+/* Saves the contents of filter_rules to localStorage.
+ * Note that IDs are excluded, since they frequently change. */
+function saveRules() {
+    var copy = Object.assign({}, filter_rules);
+    delete copy.id;
+
+    localStorage.setItem("cf_rules", JSON.stringify(copy));
+}
+
+/**********************************************************
+ * OUTPUT FUNCTIONS
+ *********************************************************/
+
+function textOut(message) {
+    addChat(null, 0, "user", "[ ChatFilter ]", message, "Filter", false, false, false, "#333", getDate());
+}
+
+function htmlOut(message) {
+    addChat(null, 0, "user", "[ ChatFilter ]", message, "Filter", true, false, false, "#333", getDate());
+}
+
+/**********************************************************
+ * CLIENT COMMANDS:
+ *     * /block list
+ *           lists all enabled blocks      
+ *     * /block (string|nick|user|id) [value]
+ *           block according to provided type and value
+ *     * /unblock (string|nick|user|id) [value]
+ *           same syntax as /block, but it removes it
+ *     * /unblock all
+ *           removes all blocks
+ *********************************************************/
+const CF_RETURNS = {
+    ALREADY_EXISTS: 0,
+    RULES_CHANGED: 1,
+    DOES_NOT_EXIST: 2,
+    CUSTOM_OUTPUT: 3
+}
+
+var block_subcommands = {
+    string: (args) => {
+        var str = args.join(" ");
+        if (filter_rules.string.indexOf(str) === -1) {
+            filter_rules.string.push(str);
+            return CF_RETURNS.RULES_CHANGED;
+        } else {
+            return CF_RETURNS.ALREADY_EXISTS;
+        }
+    },
+    nick: (args) => {
+        var name = args.join(" ");
+        if (filter_rules.nick.indexOf(name) === -1) {
+            filter_rules.nick.push(name);
+            return CF_RETURNS.RULES_CHANGED;
+        } else {
+            return CF_RETURNS.ALREADY_EXISTS;
+        }
+    },
+    user: (args) => {
+        var usr = args.join(" ");
+        if (filter_rules.user.indexOf(usr) === -1) {
+            filter_rules.user.push(usr);
+            return CF_RETURNS.RULES_CHANGED;
+        } else {
+            return CF_RETURNS.ALREADY_EXISTS;
+        }
+    },
+    id: (args) => {
+        var id = parseInt(args[0]);
+        if (Number.isNaN(id)) {
+            textOut("Invalid ID.");
+            return CF_RETURNS.CUSTOM_OUTPUT;
+        }
+
+        if (filter_rules.id.indexOf(id) === -1) {
+            filter_rules.id.push(id);
+            return CF_RETURNS.RULES_CHANGED;
+        } else {
+            return CF_RETURNS.ALREADY_EXISTS;
+        }
+    },
+    list: (_) => {
+        var out = "Your block list:<br>";
+
+        out += `=== Strings (${filter_rules.string.length})<br>`;
+        for (const i of filter_rules.string) {
+            out += `  * ${html_tag_esc(i)}<br>`;
+        }
+
+        out += `=== Nicknames (${filter_rules.nick.length})<br>`;
+        for (const i of filter_rules.nick) {
+            out += `  * ${html_tag_esc(i)}<br>`;
+        }
+
+        out += `=== Users (${filter_rules.user.length})<br>`;
+        for (const i of filter_rules.user) {
+            out += `  * ${html_tag_esc(i)}<br>`;
+        }
+
+        out += `=== IDs (${filter_rules.id.length})<br>`;
+        for (const i of filter_rules.id) {
+            out += `  * ${html_tag_esc(i.toString())}<br>`;
+        }
+
+        htmlOut(out);
+        return CF_RETURNS.CUSTOM_OUTPUT;
+    }
+};
+
+var unblock_subcommands = {
+    string: (args) => {
+        var str = args.join(" ");
+        var idx = filter_rules.string.indexOf(str);
+        if (idx === -1) return CF_RETURNS.DOES_NOT_EXIST;
+
+        filter_rules.string.splice(idx, 1);
+        return CF_RETURNS.RULES_CHANGED;
+    },
+    nick: (args) => {
+        var name = args.join(" ");
+        var idx = filter_rules.nick.indexOf(name);
+        if (idx === -1) return CF_RETURNS.DOES_NOT_EXIST;
+
+        filter_rules.nick.splice(idx, 1);
+        return CF_RETURNS.RULES_CHANGED;
+    },
+    user: (args) => {
+        var usr = args.join(" ");
+        var idx = filter_rules.user.indexOf(usr);
+        if (idx === -1) return CF_RETURNS.DOES_NOT_EXIST;
+
+        filter_rules.user.splice(idx, 1);
+        return CF_RETURNS.RULES_CHANGED;
+    },
+    id: (args) => {
+        var id = parseInt(args[0]);
+        if (Number.isNaN(id)) {
+            textOut("Invalid ID.")
+            return CF_RETURNS.CUSTOM_OUTPUT;
+        }
+
+        var idx = filter_rules.id.indexOf(id);
+        if (idx === -1) return CF_RETURNS.DOES_NOT_EXIST;
+
+        filter_rules.id.splice(idx, 1);
+        return CF_RETURNS.RULES_CHANGED;
+    },
+    all: (_) => {
+        filter_rules = JSON.parse(filter_rules_defaults);
+        saveRules();
+        textOut("Block list has been cleared.");
+        return CF_RETURNS.CUSTOM_OUTPUT;
+    }
+};
+
+// Add the extented /block command to chat.
+client_commands = Object.assign(client_commands, {
+    block: (args) => {
+        if (args.length < 1) {
+            textOut("No subcommand provided.");
+            return;
+        }
+
+        if (block_subcommands[args[0]] !== undefined) {
+            var ret = block_subcommands[args[0]](args.slice(1));
+            switch (ret) {
+                case CF_RETURNS.RULES_CHANGED:
+                    if (CF_PERSIST) saveRules();
+                    textOut("Rule has been added.")
+                    break;
+                case CF_RETURNS.ALREADY_EXISTS:
+                    textOut("Rule already exists.");
+                    break;
+            }
+        } else {
+            textOut("Unknown subcommand.")
+        }
+    },
+    unblock: (args) => {
+        if (args.length < 1) {
+            textOut("No subcommand provided.");
+            return;
+        }
+
+        if (unblock_subcommands[args[0]] !== undefined) {
+            var ret = unblock_subcommands[args[0]](args.slice(1));
+            switch (ret) {
+                case CF_RETURNS.RULES_CHANGED:
+                    if (CF_PERSIST) saveRules();
+                    textOut("Rule has been removed.")
+                    break;
+                case CF_RETURNS.DOES_NOT_EXIST:
+                    textOut("Rule does not exist.");
+                    break;
+            }
+        } else {
+            textOut("Unknown subcommand.")
+        }
+    }
+});
+
+/**********************************************************
+ * MAIN CHAT FILTERING CODE
+ *********************************************************/
 
 /* This purpose of this one should be pretty obvious given the name.
  * It looks at the currently defined rules to determine if a message
  * should be hidden. */
 function shoudHide(event) {
     // Case-insensitive check for disallowed strings.
-    for (const i in filter_rules.string) {
-        if (event.message.toLowerCase().includes(
-            filter_rules.string[i].toLowerCase())) {
+    for (const i of filter_rules.string) {
+        if (event.message.toLowerCase().includes(i.toLowerCase())) {
             return true;
         }
     }
 
     // Case-sensitive check for disallowed nicknames.
-    for (const i in filter_rules.nick) {
-        if (event.nickname === filter_rules.nick[i]) {
+    for (const i of filter_rules.nick) {
+        if (event.nickname === i) {
             return true;
         }
     }
 
     // Check for disallowed users.
-    for (const i in filter_rules.user) {
-        if (event.realUsername === filter_rules.user[i]) {
+    for (const i of filter_rules.user) {
+        if (event.realUsername === i) {
             return true;
         }
     }
 
     // Check for disallowed IDs.
-    for (const i in filter_rules.id) {
-        if (event.id === filter_rules.id[i]) {
+    for (const i of filter_rules.id) {
+        if (event.id === i) {
             return true;
         }
     }
@@ -58,7 +266,7 @@ function shoudHide(event) {
 /* Adds an anonymous function as a chatmod event listener, which calls
  * shouldHide() to determine if a message should be hidden or not. */
 w.on("chatmod", (chat_event) => {
-  if (shoudHide(chat_event)) {
-    chat_event.hide = true;
-  }
+    if (shoudHide(chat_event)) {
+        chat_event.hide = true;
+    }
 });
