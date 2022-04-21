cb88b3b06979d4b3baf81fb0d85413e7a4ba6c5e
tlras <14915607+tlras@users.noreply.github.com>
2022-04-20 22:58:46 -0500

    Update idgrinder.js

--- scripts/idgrinder.js
+++ scripts/idgrinder.js
@@ -1,64 +1,84 @@
-/* yagton's "ID Grinder" script (version 5)
- * NOTICE: While running this script, your OWOT tab will be effectively frozen
- *         until it's finished, and any chat messages/edits made during this
- *         time may not be received/sent by your client.
+/* yagton's ID Grinder (version 6)
+ * 
+ * I completely rewrote this from scratch because the original was so bad.
+ * Here's a list of changes from the original version:
+ *    - No longer freezes the whole tab until done.
+ *    - Informs you in chat when it is done.
+ *    - Has functions to easily stop and restart the grinder.
+ *        - idg.stop() and idg.grind()
  * 
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to <http://unlicense.org/> */
 
-// Keeps track of program state to prevent two copies from running at the
-// same time. Set idg_state to -1 to stop the script prematurely.
-var idg_state = idg_state || 0;
-
-// This function is used to determine if an ID is to be kept. For most people,
-// it's the only part of the script you should need to modify. By default, it
-// returns true if the ID is in the COOL_IDS array or is less than 100.
-var COOL_IDS = [420, 6969, 666, 1337, 2021];
-function isGood(a) {
-    return COOL_IDS.includes(a) || a < 100;
+/* This is the part of the code that determines whether or not an ID
+ * should be kept or not. Overwrite this with the logic you want.
+ * The example code below will simply keep any ID below 100.
+ * 
+ * If true is returned, it is kept; otherwise it is discarded. */
+function isGood(id) {
+    return id < 100;
 }
 
-// Tip: If you want to start the script again after you've loaded it once,
-// you can just rerun this function; no need to load the whole script again.
-function grindID() {
-    // Set reconnectTimeout to 0 to improve speed.
-    var oldTimeout = socket.reconnectTimeout;
-    socket.reconnectTimeout = 0;
-    
-    // Make sure there aren't any other instances.
-    if (idg_state != 0) {
-        console.log("An instance of this script is already running! Refusing to start.");
-        return;
-    }
-    idg_state = 1;
+/* The idg namespace that all the primary logic is stored inside of.
+ *
+ * The casual user does not need to worry about how any of this works,
+ * but feel free to take a peek anyway if you're curious. */
+let idg = (() => {
+    let state_t = makeEnum(['running', 'halt', 'off']);
+    let state = state_t.off;
+    let sock = null;  // hehe sock
 
-    // Proxies ws_functions.channel() to intercept when a new ID is received from
-    // the server. If the ID is approved by isGood(), the proxy is removed and
-    // the original function then runs normally; but if it's not approved, it will
-    // run socket.refresh() again and again until it eventually is.
-    var old_function = ws_functions.channel;
-
-    ws_functions.channel = new Proxy(ws_functions.channel, {
-        apply: function(target, _, args) {
-            if (isGood(args[0].id) || idg_state == -1) {
-                console.log(`Stopping! Your new ID is: ${args[0].id}`);
-                idg_state = 0;
-                
-                // Restore environment to what it was previously set to.
-                ws_functions.channel = old_function;
-                socket.reconnectTimeout = oldTimeout;
-                
-                return target(...args);
-            } else {
-                socket.refresh();
-            }
+    // A function that starts the ID grinding process.
+    function startGrinding() {
+        // If the grinder has been halted, respond accordingly.
+        if (state === state_t.halt) {
+            state = state_t.off;
+            sock = null;
+            return;
         }
-    });
 
-    // Generates a new ID, and due to the code above, it will keep rerunning until
-    // you get your desired ID.
-    socket.refresh();
-}
+        // Create a new socket;
+        if (sock !== null) sock.close();
+        sock = new WebSocket(ws_path);
 
-// Begin grinding a new ID.
-grindID();
+        // Add some methods to it.
+        sock.onmessage = (e) => {
+            let data = JSON.parse(e.data);
+
+            // If it worked, then inform the user and halt.
+            if (data.kind === "channel" && isGood(data.id)) {
+                clientChatResponse(`Got ID ${data.id}, stopping!`);
+
+                // Make the socket "usable" and replace the default socket with it.
+                [sock.onmessage, sock.onerror] = [undefined, undefined];
+                socket.socket = sock;
+                state = state_t.halt;
+            }
+
+            // Call this function again to replace itself.
+            startGrinding();
+        };
+
+        sock.onerror = startGrinding;
+    }
+
+    // The idg.stop() and idg.grind() functions; pretty self-explanatory.
+    return {
+        stop: () => {
+            if (state !== "running")
+                throw "IDG is not running, cannot halt!";
+            
+            clientChatResponse("Halting due to user request.");
+            state = state_t.halt;
+        },
+        grind: () => {
+            if (state !== state_t.off)
+                throw "Cannot start if already running!";
+            
+            startGrinding();
+        }
+    }
+})();
+
+// Start the process; omit this line if you don't want it to start instantly.
+idg.grind();
