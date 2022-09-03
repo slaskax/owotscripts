3125c65c6226f97297003408021936dfc89a681d
tlras <14915607+tlras@users.noreply.github.com>
2022-09-03 00:38:52 -0500

    Simplify cursor logic and add ESC support.

--- scripts/imageeraser.js
+++ scripts/imageeraser.js
@@ -1,11 +1,11 @@
-/* yagton's "Image Eraser" script (version 2)
- * Easy to use: just press Ctrl+E, click the image, and it's gone!
- *
- * ACKNOWLEDGEMENT: Setting flush interval to 0 in order to improve
- *                  speed was Nitsua's idea. Thanks for your contribution!
- *
- * This is free and unencumbered software released into the public domain.
- * For more information, please refer to <http://unlicense.org/> */
+/* yagton's "Image Eraser" script (version 3)
+* Easy to use: just press Ctrl+E, click the image, and it's gone!
+*
+* ACKNOWLEDGEMENT: Setting flush interval to 0 in order to improve
+*                  speed was Nitsua's idea. Thanks for your contribution!
+*
+* This is free and unencumbered software released into the public domain.
+* For more information, please refer to <http://unlicense.org/> */
 
 const ERASE_KB = "CTRL+E";
 let erase_mode = false;
@@ -26,9 +26,12 @@ function flood(x, y) {
 }
 
 w.on("keyDown", e => {
-    if (!checkKeyPress(e, ERASE_KB)) return;
-    erase_mode = !erase_mode;
-    owot.style.cursor = defaultCursor = erase_mode ? "crosshair" : "text";
+    if (checkKeyPress(e, keyConfig.reset))
+        erase_mode = false;
+    else if (checkKeyPress(e, ERASE_KB))
+        erase_mode = !erase_mode;
+
+    owot.style.cursor = erase_mode ? "crosshair" : defaultCursor;
 });
 
 w.on("mouseDown", e => {
@@ -37,7 +40,6 @@ w.on("mouseDown", e => {
         y = (e.tileY * tileR) + e.charY;
 
     erase_mode = false;
-    owot.style.cursor = defaultCursor = "text";
     flood(x, y);
 });
 
