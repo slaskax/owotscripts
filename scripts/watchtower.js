bab0c85cf24702ae63108cf16e3117522e3253d9
tlras <14915607+tlras@users.noreply.github.com>
2023-02-23 08:34:12 -0600

    Update watchtower.js

--- scripts/watchtower.js
+++ scripts/watchtower.js
@@ -1,10 +1,10 @@
-/* yagton's "Watchtower" script (version 3)
+/* yagton's "Watchtower" script (version 3.1)
  * This is a simple script listens on the active WebSocket for tileUpdate messages.
  * After it recieves such a message, it outputs the location of the update to the
  * console, or as a notification if the menu option is enabled. The primary purpose
  * of this script is so you can see where edits are happening on an OWOT world.
  * Please do not use this for malicious purposes.
- * 
+ *
  * NOTICES:
  *     1. This script has a known "off-by-one" bug. I have tried several times to fix it,
  *        but have not succeeded. If you find out how to fix it, *please* let me know.
@@ -14,7 +14,7 @@
  *        nefarious purposes, I have decided to not release it publicly, but rather
  *        privately to a select few who I deem safe. The public version of Watchtower
  *        will be staying at version v3 indefinitely with the exception of bug fixes.
- * 
+ *
  * CONTRIBUTIONS:
  *     - Vince0110 (added notifications)
  *
@@ -23,34 +23,27 @@
 // "tileUpdate notifications" menu option
 var notifsEnabled = false;
 
-menu.addCheckboxOption("tileUpdate notifications",
-  function () {notifsEnabled = true},
-  function () {notifsEnabled = false},
-  false
+menu.addCheckboxOption(
+    "tileUpdate notifications",
+    () => notifsEnabled = true,
+    () => notifsEnabled = false,
+    false
 );
 
-// Proxies the active WebSocket.
-w.socket.onmessage = new Proxy(w.socket.onmessage, {
-  apply: function(func, _, args) {
-    var data = JSON.parse(args[0].data);
-    if (data.kind == "tileUpdate") {
-      for (const i in data.tiles) {
+// Enables a tileUpdate listener.
+w.on("tileUpdate", (e) => {
+    for (const i in e.tiles) {
         // Some math to convert server-retrieved coords to actual coords.
-        var nums = i.split(',').map(x => parseInt(x));
-        var positionX = Math.floor(nums[1] / coordSizeX);
-        var positionY = Math.floor(-nums[0] / coordSizeY);
+        let nums = i.split(",").map((x) => parseInt(x));
+        let positionX = Math.floor(nums[1] / coordSizeX);
+        let positionY = Math.floor(-nums[0] / coordSizeY);
 
-        var output = `tileUpdate at ${positionX}, ${positionY}.`;
-        console.log(output);
+        console.log(`tileUpdate at ${positionX}, ${positionY}.`);
         if (notifsEnabled) {
-          if (Notification.permission !== "granted") {
-            Notification.requestPermission();
-          }
-          new Notification(output, {tag: 'tileUpdate'});
+            if (Notification.permission !== "granted") {
+                Notification.requestPermission();
+            }
+            new Notification(output, { tag: "tileUpdate" });
         }
-      }
     }
-
-    func(...args);
-  }
 });
