1923b16479d2979eeb2fc06743292183a730e2bc
tlras <14915607+tlras@users.noreply.github.com>
2023-03-04 10:20:33 -0600

    Update newsticker.js

--- scripts/newsticker.js
+++ scripts/newsticker.js
@@ -1,10 +1,14 @@
 /* yagton's "News Ticker" script (version 1.3)
-* I saw a neat news ticker on OWOT one day, so I decided to implement
-* my own public-domain version. Change the variables prefixed with
-* `news_` to change the location, width, and contents of the ticker.
-*
-* This is free and unencumbered software released into the public domain.
-* For more information, please refer to <http://unlicense.org/> */
+ * I saw a neat news ticker on OWOT one day, so I decided to implement
+ * my own public-domain version. Change the variables prefixed with
+ * `news_` to change the location, width, and contents of the ticker.
+ *
+ * NOTE: In order to easily set the location, you can enable cursor
+ *       coords by clicking the coordinates bar; to enable the bar,
+ *       open the menu in the top right and select it.
+ *
+ * This is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org/> */
 
 let news_location = [-1, -2, 0, 2];
 let news_width = 32;
